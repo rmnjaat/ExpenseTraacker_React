7192cf0769fab1ad5a2039b6799c97807afd1908
rmnjaat <ramanjangu01@outlook.com>
2023-12-19 16:51:30 +0530

    prop completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+
+import Expenses from './components/Expenses/Expenses.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let expenseDate= new Date(2021,3,28);
+    let expenseTitle ="School Fee";
+    let expenseAmount=300; 
+
+    let expenses =[
+        {
+            id:'e1',
+            title:'school fee',
+            amount:250,
+            date: new Date(2021,4,28)
+        },
+        {
+            id:'e2',
+            title:'books ',
+            amount:50,
+            date: new Date(2021,3,28)
+        },
+        {
+            id:'e3',
+            title:' fair',
+            amount:30,
+            date: new Date(2021,6,18)
+        },
+        {
+            id:'e4',
+            title:'GYM fee',
+            amount:180,
+            date: new Date(2021,7,21)
+        },
+    ];
+
+
+    return(
+     <div>
+
+            <h2>hiii hello</h2>
+
+            <Expenses item={expenses} />
+
+       
+        </div>
+    );
+    
 }
 
 export default App;
